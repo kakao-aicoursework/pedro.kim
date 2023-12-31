833da4d503cbf61a03c31a10334d5479586573b6
pedro-kim <deflatedlatte@gmail.com>
2024-01-01 03:05:05 +0900

    Modify callback function to use LangChain

--- project_step2/callback.py
+++ project_step2/callback.py
@@ -11,43 +11,38 @@ from langchain.prompts import (
     HumanMessagePromptTemplate
 )
 from langchain.chains import LLMChain
+from langchain.schema import BaseOutputParser
 
 with open("kakaosync_prompt_data.json") as f:
     kakaosync_intro_document = f.read()
 
 logger = logging.getLogger("Callback")
 
+class StringOutputParser(BaseOutputParser):
+    def parse(self, text: str):
+        return text
+
 def complete_user_utterance(user_utterance):
-    chat_template = ChatPromptTemplate.from_messages(
-        [
-            SystemMessagePromptTemplate.from_template(
-                template=(
-                    "You are a service provider whose company name is "
-                    "'카카오'.  You provide a service named '카카오싱크'."
-                    "The following JSON document is a service manual for "
-                    "'카카오싱크':\n\n"
-                    "{service_manual_for_kakaosync}\n\n"
-                    "When the user asks you about '카카오싱크', you have to "
-                    "answer the question from the information provided by "
-                    "the service manual."
-                )
-            ),
-            HumanMessagePromptTemplate.from_template("{user_message}")
-        ]
-    )
-    chat = ChatOpenAI(temperature=0.8)
-    chain = LLMChain(llm=chat, prompt=chat_template)
-    return chain.run(
-        service_manual_for_kakaosync=kakaosync_intro_document,
-        user_message=user_utterance
-    )
+    chat_template = ChatPromptTemplate.from_messages([
+        SystemMessagePromptTemplate.from_template(
+            "You are a helpful assitant who works for a software service"
+            "provider named '카카오'.  Your job is to answer questions about "
+            "a service named '카카오싱크'.  Your answer must be in Korean."
+        ),
+        HumanMessagePromptTemplate.from_template("{user_message}")
+    ])
+    chain = chat_template | ChatOpenAI(temperature=0.8) | StringOutputParser()
+    return chain.invoke({
+        "service_manual_for_kakaosync": kakaosync_intro_document,
+        "user_message": user_utterance
+    })
 
 async def callback_handler(request: ChatbotRequest) -> dict:
 
     # ===================== start =================================
     completion = complete_user_utterance(request.userRequest.utterance)
 
-   # 참고링크 통해 payload 구조 확인 가능
+    # 참고링크 통해 payload 구조 확인 가능
     payload = {
         "version": "2.0",
         "template": {
@@ -64,11 +59,9 @@ async def callback_handler(request: ChatbotRequest) -> dict:
     # 참고링크1 : https://kakaobusiness.gitbook.io/main/tool/chatbot/skill_guide/ai_chatbot_callback_guide
     # 참고링크1 : https://kakaobusiness.gitbook.io/main/tool/chatbot/skill_guide/answer_json_format
 
-    time.sleep(1.0)
-
     url = request.userRequest.callbackUrl
 
     if url:
         async with aiohttp.ClientSession() as session:
             async with session.post(url=url, json=payload, ssl=False) as resp:
-                await resp.json()
+                obj = await resp.json()
